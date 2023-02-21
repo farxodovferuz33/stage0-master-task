97985b67f30853dd5904db64c241eb15e609bf73
farxodovferuz33 <119694606+farxodovferuz33@users.noreply.github.com>
2023-02-21 11:12:57 +0500

    Update Dog.java

--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -1,5 +1,10 @@
 package com.epam.OOP;
 
-public class Dog {
+public class Dog extends Animal{
+
+    @Override
+    public void getDescription(String color, int numberOfPaws, boolean hasFur) {
+        super.getDescription(color, numberOfPaws, hasFur);
+    }
 
 }
