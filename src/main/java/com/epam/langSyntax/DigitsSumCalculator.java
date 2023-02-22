7dc8c11fe3e03c9e910636fdfd487ce8002cf185
farxodovferuz33 <119694606+farxodovferuz33@users.noreply.github.com>
2023-02-22 03:16:28 -0500

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,16 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int numLast = number%10; //last digit
+        int numFirstThree = number/10; //first 3 digit
+        int numFirstThreeLast = numFirstThree%10; //first 3 digit last
+        int numFirstThreeLastFirstTwo = numFirstThree/10;  //first 3 digit first 2
+        int numFirstThreeFirstTwoLast = numFirstThreeLastFirstTwo%10;
+        int numFirst = numFirstThreeLastFirstTwo/10;
+
+        int sum = numFirst+numFirstThreeLast+numFirstThreeFirstTwoLast+numLast;
+
+        System.out.println(sum);
     }
 
 }
